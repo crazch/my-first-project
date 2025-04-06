fdf4f45e6f529eb3cdd56b1201d0a6051b901e39
Crazch <urayzainulkawakib26@gmail.com>
2025-04-06 17:45:34 +0700

    added chances feature

--- game.py
+++ game.py
@@ -2,12 +2,15 @@ import random
 
 target = random.randint(1, 100)
 
+chances = 10
 while True:
+  print(f"You Have {chances} chances left")
   try:
     guess = int(input("Enter Number(1-100): "))
   except ValueError:
       print("Invalid!")
       continue
+  chances -= 1
 
   if guess > target:
     print("Too High")
@@ -15,4 +18,8 @@ while True:
     print("Too low")
   else:
     print("You Guessed it!")
+    break
+  
+  if chances == 0:
+    print("No Chances Left")
     break
